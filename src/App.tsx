58cf7de9d045b64933a637202f710da5595eb198
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-11-10 19:11:44 -0500

    Remove unused Login component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import './App.css';
 import { useContext } from 'react';
 import { AuthProvider, AuthContext } from './context/context';
-import Login from './components/Login';
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <Login></Login>
-    </AuthProvider>
-  );
+  return <AuthProvider></AuthProvider>;
 };
 
 export default App;
