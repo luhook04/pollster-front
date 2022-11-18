70b761974c469f1779e690bd588675d4b5e327e0
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-11-18 15:12:39 -0500

    Setup the Home component and all the possible routes for the app

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { AuthContext } from '../context/context';
+import Nav from './Nav';
 
 const Home = () => {
+  const { state } = useContext(AuthContext);
+
   return (
     <BrowserRouter basename="/">
+      {state.isAuthenticated && <Nav />}
       <Routes>
         <Route path="/" element={<div>cool</div>}></Route>
         <Route path="/newpoll" element={<div>create poll</div>}></Route>
+        <Route path="/users/:userId" element={<div>view account</div>}></Route>
       </Routes>
     </BrowserRouter>
   );
