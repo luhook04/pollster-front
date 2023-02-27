2d4c5852ade5750d05e68b3cb001fd773a15c1b6
Luke Hook <lukehook04@gmail.com>
2023-02-27 17:00:32 -0500

    Change html for when user doesn't havd any friends

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -8,20 +8,20 @@ interface FuncProps {
 const Friends: React.FC<FuncProps> = ({ friends }) => {
   return (
     <>
-      {friends ? (
-        <>
-          <h3 className="mb-2">Friends</h3>
-          <div className="h-fit max-h-36 overflow-auto bg-blue-500 rounded text-white">
-            {friends.map((friend: User) => {
-              return (
-                <p className="my-2" key={friend._id}>
-                  {friend.username}
-                </p>
-              );
-            })}
-          </div>
-        </>
-      ) : null}
+      <h3 className="mb-2">Friends</h3>
+      <div className="h-fit max-h-36 overflow-auto bg-blue-500 rounded text-white">
+        {friends.length > 0 ? (
+          friends.map((friend: User) => {
+            return (
+              <p className="my-2" key={friend._id}>
+                {friend.username}
+              </p>
+            );
+          })
+        ) : (
+          <p className="text-white my-2">No friends yet</p>
+        )}
+      </div>
     </>
   );
 };
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -94,7 +94,6 @@ const Login = () => {
             >
               Login
             </button>
-
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
               onClick={openSignup}
